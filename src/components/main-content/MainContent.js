c3bf8eba8e5d80bcb6dee93dc4ef0ece30fcde1f
Hendra Wijaya <hendra.heree@gmail.com>
2021-09-17 18:08:46 +1000

    main content half done, with media query

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./mainContent.css";
 import { Product } from "../product/Product";
-import { Container } from "react-bootstrap";
 
 export const MainContent = () => {
   return (
     <div className="main-content-container">
       <div className="main-content">
+        <Product></Product>
+        <Product></Product>
+        <Product></Product>
         <Product></Product>
       </div>
     </div>
