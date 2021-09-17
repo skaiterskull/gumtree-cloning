0282df70453fdd7c9a39d305b96ab9c66f6ed64c
Hendra Wijaya <hendra.heree@gmail.com>
2021-09-17 17:12:27 +1000

    Product component created

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./mainContent.css";
+import { Product } from "../product/Product";
+import { Container } from "react-bootstrap";
 
 export const MainContent = () => {
-  return <div></div>;
+  return (
+    <div className="main-content-container">
+      <div className="main-content">
+        <Product></Product>
+      </div>
+    </div>
+  );
 };
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,10 +4,22 @@ import product1 from "../../assests/images/product1.png";
 
 export const Product = () => {
   return (
-    <a href="">
-      <div className="product-container">
-        <div className="image-container"></div>
-      </div>
-    </a>
+    <>
+      <a href="">
+        <div className="product-container">
+          <div className="price">$1,850</div>
+          <div className="wrapper">
+            <div className="image-container">
+              <img src={product1} width="230px" height="175px" alt="" />
+            </div>
+            <div className="product-description">Power Mobility Chair</div>
+            <div className="location-and-fav">
+              <span>Launceston</span>
+              <span>♥</span>
+            </div>
+          </div>
+        </div>
+      </a>
+    </>
   );
 };
