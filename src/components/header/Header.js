759fa62a0e902cbb8cea08417d17a00f07a12aa5
Hendra Wijaya <hendra.heree@gmail.com>
2021-09-19 17:11:17 +1000

    header almost done, minus search bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./header.css";
+import bg from "../../assests/images/header-image.webp";
 
 export const Header = () => {
   return (
     <div className="header-container">
-      <div className="header-wrapper"></div>
+      <img className="header-image" src={bg} />
+      <div className="header-menu">
+        <a href="">
+          <span className="header-menu-left">🌳 Gumtree</span>
+        </a>
+        <button className="header-menu-right">Add an ad</button>
+      </div>
+      <div className="middle-text">
+        <div className="middle-text-left">Trade Together, Save Together</div>
+        <span className="middle-text-right">Proud Partner of PLANET ARK</span>
+      </div>
     </div>
   );
 };
